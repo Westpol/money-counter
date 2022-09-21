a26de8ad87af362e20fa0bbfef04642d093f0bf7
Westpol <81779508+Westpol@users.noreply.github.com>
2022-09-21 18:59:37 +0200

    lul

--- count_money.py
+++ count_money.py
@@ -1,6 +1,2 @@
-import time
-
-
 def count_the_money():
     print("counting the money")
-    time.sleep(12)
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import json
-from count_money import count_the_money
+import count_money
 import add_to_balance
+
+coins = '{"twoeuro":0, "oneeuro":0, "fiftycent":0, "twentycent":0, "tencent":0, "fivecent":0, "twocent":0, "onecent":0}'
+paper = '{"fiveeuro":0, "teneuro":0, "twentyeuro":0, "fiftyeuro":0, "onehundredeuro":0, "twohundrereuro":0, "fivehundrereuro":0}'
+
+jsoncoins = json.loads(coins)
+jsonpaper = json.loads(paper)
+
+jsoncoins["oneeuro"] = 3
+
+print(jsoncoins["oneeuro"])
 
 while 1:
     mode = input("What do you want to do?\n 1: Count money from 0€\n 2: Add to your current balance")
     if mode.isdigit():
+        mode = int(mode)
         if mode == 1:
-            count_the_money()
+            count_money.count_the_money()
         elif mode == 2:
             add_to_balance.add_the_balance()
     else:
